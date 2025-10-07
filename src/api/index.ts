44a7190f85b77a959c2720e774b52d5553a193f5
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-10-07 15:28:46 +0530

    add vercel/node

---
+++ src/api/index.ts
@@ -0,0 +1,5 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../app";
+
+// Vercel serverless function
+export default (req: VercelRequest, res: VercelResponse) => app(req, res);
