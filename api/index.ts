73ed27c9f1ccb40ad6ec43bbf03a4a6cecffbc1b
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-10-07 15:40:53 +0530

    move api to root

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../app";
+import app from "../src/app";
 
 // Vercel serverless function
 export default (req: VercelRequest, res: VercelResponse) => app(req, res);
