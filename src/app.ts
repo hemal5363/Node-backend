c90053bda939d9cfd3e9928e9953b407cb059a03
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-10-07 16:55:15 +0530

    add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 app.use(express.json());
 
