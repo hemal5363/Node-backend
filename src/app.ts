fd31aa21cea90b3651d4769a482994c104062fa6
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-10-07 17:42:54 +0530

    add user models

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import User from "./models/User";
 
 const app = express();
 
@@ -9,6 +10,24 @@ app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express + Vercel + Local!");
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.status(201).json(users);
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
 });
 
 export default app;
