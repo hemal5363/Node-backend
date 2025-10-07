34dc75e93de7679ee978b7f96910daa904ecedd0
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-10-07 15:52:58 +0530

    update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,4 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import serverless from "serverless-http";
 import app from "../src/app";
 
-// Wrap Express app for Vercel serverless
-const handler = serverless(app);
-
-export default (req: VercelRequest, res: VercelResponse) => handler(req, res);
+export default serverless(app);
