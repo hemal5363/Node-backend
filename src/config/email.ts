751b98aa8e0ea34211b4f73d1d945920a036fb13
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-11-19 09:42:08 +0530

    Add console for email

--- src/config/email.ts
+++ src/config/email.ts
@@ -2,15 +2,23 @@ import nodemailer from "nodemailer";
 
 import { EmailOptions } from "../types/email";
 
-const sendEmail = async (options: EmailOptions) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+transporter
+  .verify()
+  .then(() => {
+    console.log("Ready for send emails");
+  })
+  .catch((error) => {
+    console.log(error);
   });
+const sendEmail = async (options: EmailOptions) => {
   const message = {
     from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
     to: options.email,
